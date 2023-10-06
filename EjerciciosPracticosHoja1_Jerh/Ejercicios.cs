14dd63ea82f2f5e223357538961a20615e824f1f
jerh011 <jeelrohe@gmail.com>
2023-10-06 04:46:02 -0700

    Agregar archivos de proyecto.

---
+++ EjerciciosPracticosHoja1_Jerh/Ejercicios.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosPracticosHoja1_Jerh
+{
+    internal class Ejercicios
+    {
+        public int[] Sumas(int[] a, int[] b)
+        {
+            int[] c = new int[a.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                c[i] = a[i] + b[i];
+            }
+            return c;
+        }
+        public void ImprecionesArray(int[] a, int[] b, int[] c)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine($"{a[i]}+{b[i]} = {c[i]}");
+            }
+        }
+        public void ImprecionesArra2( int[] c)
+        {
+            for (int i = 0; i < c.Length; i++)
+            {
+              
+                Console.WriteLine($"La Pociiocion es {c}");
+            }
+        }
+        public int[] Buscar(string Elemento,string[] array_1 )
+        {
+            int[] array_2 = new int[array_1.Length];
+            int x = 0;
+            for (int i = 0; i < array_1.Length; i++)
+            {
+                if (array_1[i] == Elemento)
+                {
+                    array_2[x] = i;
+                x++;
+                }
+            }
+            return array_2;
+        }
+
+    }
+}
---
+++ EjerciciosPracticosHoja1_Jerh/Program.cs
@@ -0,0 +1,55 @@
+﻿using EjerciciosPracticosHoja1_Jerh;
+
+int[] a = new int[5];
+int[] b = new int[a.Length];
+int[] c = new int[a.Length];
+Ejercicios ejercicios = new Ejercicios();
+Random random = new Random();
+
+for (int i = 0; i < a.Length; i++)
+{ 
+    a[i] = random.Next(100);
+
+    b[i] = random.Next(100);
+}
+
+c = ejercicios.Sumas(a,b);
+ejercicios.ImprecionesArray(a,b,c);
+/*
+Console.WriteLine("===============================================");
+string[] array_1 = { "Hola", "como","estan","Fasil" };
+int[] array_2 = new int[array_1.Length];
+for (int i = 0;i < array_1.Length;i++)
+    Console.WriteLine(array_1[i]);
+Console.WriteLine("Busque el El elemento");
+string Elemento=Console.ReadLine();
+
+array_2=ejercicios.Buscar(Elemento, array_1);
+ejercicios.ImprecionesArra2(array_2);
+*/
+Console.WriteLine("===============================================");
+int[,] Alumnos = new int[10,3];
+
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        // Generar un número aleatorio entre 0 y 100 y asignarlo a la posición (i, j)
+        Alumnos[i, j] = random.Next(10); // 101 se utiliza para incluir el 100
+    }
+}
+
+Console.WriteLine("  Alumnos  |Algebra    |analisis   |");
+    Console.Write("-----------+-----------+-----------+");
+Console.WriteLine(" ");
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        // Generar un número aleatorio entre 0 y 100 y asignarlo a la posición (i, j)
+        Console.Write("  {0,-9}|",Alumnos[i, j]);  // 101 se utiliza para incluir el 100
+        
+    }
+    Console.WriteLine(" ");
+    Console.WriteLine("-----------+-----------+-----------+");
+}
